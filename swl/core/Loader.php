3b0d22c8dae5cd7e49b001daf5a5765de9716a13
Ubuntu <ubuntu@ip-10-0-0-249.ec2.internal>
2014-11-13 20:05:41 +0000

    Loader

--- swl/core/Loader.php
+++ swl/core/Loader.php
@@ -40,11 +40,13 @@ class Loader
 
         $file = $newPath . '.php';
 
-        if ((!\is_file($file) || \is_dir($file)) && \stripos($path, '\\') !== false)
+	$pattern = '/^(\\)?(SebastianBergmann)/';
+
+        if (!preg_match($pattern, $path) && (!\is_file($file) || \is_dir($file)) && \stripos($path, '\\') !== false)
         {
             throw new Exception("Não foi possível encontrar a definição de objeto '{$path}'");
         }
-        else if (\stripos($path, '\\') !== false)
+        else if (\stripos($path, '\\') !== false && !preg_match($pattern, $path))
         {
             require_once $file;
         }
